da17a752d1cc0c938ebcd4f62a2093eaf20ebf6b
Jan Ranostaj <HonzaRanostaj@gmail.com>
2019-06-02 14:48:33 +0200

    add gym model validations

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,3 +1,9 @@
 class Gym < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :email, presence: true
+  validates :description, presence: true
 end
