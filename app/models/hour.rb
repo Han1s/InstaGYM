72c89b61a315bdd257584b93952767cb1aaf95e1
Jan Ranostaj <HonzaRanostaj@gmail.com>
2019-06-07 20:04:09 +0200

    make validations

--- app/models/hour.rb
+++ app/models/hour.rb
@@ -1,3 +1,7 @@
 class Hour < ApplicationRecord
   belongs_to :gym
+
+  validates :day,          presence: true, inclusion: { in: (1..7)  }
+  validates :opening_time, presence: true, inclusion: { in: (0..24) }
+  validates :closing_time, presence: true, inclusion: { in: (0..24) }
 end
