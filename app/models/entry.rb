f5943344be86d48f3c3b067a5c9309f0b3b34312
Jan Ranostaj <HonzaRanostaj@gmail.com>
2019-06-07 22:16:26 +0200

    add validations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,7 @@
 class Entry < ApplicationRecord
   belongs_to :gym
+
+  validates :number_of_entries, presence: true
+  validates :duration,          presence: true
+  validates :position,          presence: true, inclusion: { in: (1..3) }
 end
