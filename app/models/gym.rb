c5733f17ba5f9eb5edc68364c81e8641d01f7e24
Jan Ranostaj <HonzaRanostaj@gmail.com>
2019-06-07 19:43:09 +0200

    remove faulty opening hour migration

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,6 +1,6 @@
 class Gym < ApplicationRecord
   belongs_to :user, class_name: "User", foreign_key: "user_id"
-  has_many :opening_hours, class_name: "OpeningHour", foreign_key: "gym_id"
+  # has_many :opening_hours, class_name: "OpeningHour", foreign_key: "gym_id"
 
   validates :name, presence: true
   validates :address, presence: true
--- app/models/opening_hour.rb
+++
@@ -1,7 +0,0 @@
-class OpeningHour < ApplicationRecord
-  belongs_to :gym, class_name: "Gym", foreign_key: "gym_id"
-
-  validates :week_day, presence: true, inclusion: { in: 1..7 }
-  validates :opening_time, presence: true
-  validates :closing_time, presence: true
-end
